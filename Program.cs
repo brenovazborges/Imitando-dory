41bbfa8ff8e0cb38602882fb16b9a70eec9db750
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-22 17:41:27 -0300

    atividade finalizada

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("--- tradutor de Baleês ---");
+Console.WriteLine("didite o texto a ser traduzido: ");
+string texto = Console.ReadLine();
+Console.WriteLine("\nA Dori diria... \n");
+
+Console.WriteLine(texto
+  .Replace("a", "aaa")
+  .Replace("e", "eee")
+  .Replace("i", "iii")
+  .Replace("o", "ooo")
+  .Replace("u", "uuu")
+  .Replace("A", "AAA")
+  .Replace("E", "EEE")
+  .Replace("I", "III")
+  .Replace("O", "OOO")
+  .Replace("U", "UUU")
+);
+Console.WriteLine();
